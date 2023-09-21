a37758abb1dce43bdaa21acdd319c2db611a5a31
Sal Ceja <salceja45@gmail.com>
2023-09-21 13:25:47 -0700

    initial commit

---
+++ apiFunctions.js
@@ -0,0 +1,17 @@
+function viewAllDeparments() {}
+function viewAllRoles() {}
+function viewAllEmployees() {}
+function addADepartment() {}
+function addARole() {}
+function addEmployee() {}
+function updateEmployee() {}
+
+module.exports = {
+  viewAllDeparments,
+  viewAllRoles,
+  viewAllEmployees,
+  addADepartment,
+  addARole,
+  addEmployee,
+  updateEmployee,
+};
